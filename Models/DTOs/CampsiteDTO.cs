1e0214db2c9881957712a6e86724cd525d07d9ec
Charles Seaton <seatonct@gmail.com>
2024-02-21 12:30:16 -0600

    added Models and DTOs

---
+++ Models/DTOs/CampsiteDTO.cs
@@ -0,0 +1,11 @@
+namespace CreekRiver.Models.DTOs;
+
+public class CampsiteDTO
+{
+    public int Id { get; set; }
+    public string Nickname { get; set; }
+    public string ImageUrl { get; set; }
+    public int CampsiteTypeId { get; set; }
+    public CampsiteType CampsiteType { get; set; }
+    public List<Reservation> Reservations { get; set; }
+}
